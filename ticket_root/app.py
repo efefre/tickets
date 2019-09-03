df30c67b5030cdfaa1512e55e1b0d89d9e359a89
Kasia <efefre@icloud.com>
2019-09-03 22:18:05 +0200

    Add cancel_date to class: Ticket

--- ticket_root/app.py
+++ ticket_root/app.py
@@ -2,10 +2,12 @@ class Ticket:
     #handling fee is 20% but not more than 50 zl
     handling_fee_percent = 0,2
 
-    def __init__(self, start_date = None, day = 0, end_date = None):
+
+    def __init__(self, start_date = None, day = 0, end_date = None, cancel_date = None):
         self.start_date = start_date
         self.day = day
         self.end_date = end_date
+        self.cancel_date = cancel_date
         self.money_back = None
 
     def __str__(self):
