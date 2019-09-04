670e1fdc732004775e7afb07e03c1eddeff6c8fe
Kasia <efefre@icloud.com>
2019-09-04 22:25:49 +0200

    Convert start_date into datetime object

--- ticket_root/app.py
+++ ticket_root/app.py
@@ -9,7 +9,7 @@ class Ticket:
     handling_fee_percent = 0.2
 
     def __init__(self, start_date = None, day = 0, end_date = None, cancel_date = None, ticket_price = None):
-        self.start_date = start_date
+        self.start_date = convert_date(start_date)
         self.day = day
         self.end_date = end_date
         self.cancel_date = cancel_date
