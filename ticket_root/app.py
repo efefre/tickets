97731559753e15820045ec6b7612d2380adbb009
Kasia <efefre@icloud.com>
2019-09-04 21:47:51 +0200

    Add ticket price to class: Ticket

--- ticket_root/app.py
+++ ticket_root/app.py
@@ -2,13 +2,13 @@ class Ticket:
     #handling fee is 20% but not more than 50 zl
     handling_fee_percent = 0,2
 
-
-    def __init__(self, start_date = None, day = 0, end_date = None, cancel_date = None):
+    def __init__(self, start_date = None, day = 0, end_date = None, cancel_date = None, ticket_price = None):
         self.start_date = start_date
         self.day = day
         self.end_date = end_date
         self.cancel_date = cancel_date
         self.money_back = None
+        self.ticket_price = ticket_price
 
     def __str__(self):
         return 'New ticket: {} - {} (days: {})'.format(self.start_date, self.end_date, self.day)
