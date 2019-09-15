b5cb921c9a306dd2283f7d4ee5466a99593a9d55
Kasia <efefre@icloud.com>
2019-09-15 17:02:52 +0200

    Add test: create_object

--- ticket_root/test/test.py
+++ ticket_root/test/test.py
@@ -5,3 +5,11 @@ from ticket_root.app import Ticket
 def test_convert_date():
     data = Ticket.convert_date('01-09-2019')
     assert data == datetime.date(2019,9,1)
+
+def test_create_object():
+    new_ticket = Ticket('01-09-2019','30',None,'15-09-2019','100')
+    assert new_ticket.start_date == '01-09-2019'
+    assert new_ticket.day == '30'
+    assert new_ticket.end_date == None
+    assert new_ticket.cancel_date == '15-09-2019'
+    assert new_ticket.ticket_price == '100'
