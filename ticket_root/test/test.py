4b8e5e2071678fd5ce4addae0a34adc2874dcaf8
Kasia <efefre@icloud.com>
2019-09-15 20:00:00 +0200

    Add test for method: count_money_back
    - use pytest parametrize: 4 use cases (2 x correct data, 2 x wrong data)

--- ticket_root/test/test.py
+++ ticket_root/test/test.py
@@ -1,4 +1,5 @@
 import datetime
+import pytest
 from ticket_root.app import Ticket
 
 
@@ -13,3 +14,20 @@ def test_create_object():
     assert new_ticket.end_date == None
     assert new_ticket.cancel_date == '15-09-2019'
     assert new_ticket.ticket_price == '100'
+
+
+@pytest.mark.parametrize('start_date, days,end_date, cancel_date, price,result',
+                         [
+                             ('01-09-2019',30,'30-09-2019','15-09-2019',100, 40),
+                             ('01-09-2019',30,'30-09-2019','30-09-2019',100, 0),
+                             ('01-09-2019',30,'30-09-2019','15-08-2019',100, 'Wprowadzono błędną datę. Nie można zwrócić biletu przed jego aktywacją'),
+                             ('01-09-2019',30,'30-09-2019','15-11-2019',100, 'Wprowadzono błędną datę. Nie można zwrócić biletu po terminie ważności')
+                         ]
+                         )
+def test_count_money_back(start_date, days,end_date, cancel_date, price,result):
+    new_ticket = Ticket(Ticket.convert_date(start_date),days,Ticket.convert_date(end_date),Ticket.convert_date(cancel_date),price)
+    if new_ticket.cancel_date < new_ticket.start_date or new_ticket.cancel_date > new_ticket.end_date:
+        assert new_ticket.count_money_back() == result
+    else:
+        new_ticket.count_money_back()
+        assert new_ticket.money_back == result
